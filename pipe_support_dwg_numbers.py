46410d99441a658d927f613c8b10b81b521373cd
Sziva Laszlo <Laszlo.Sziva@ansaldoenergia.com>
2020-09-08 10:28:42 +0200

    first

---
+++ pipe_support_dwg_numbers.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import pathlib
+import logging
+
+print ("RAN")
+
+df = pd.read_excel(r'C:\Users\50000700\Python\Python_repos\multi-purpose-excel-wizardry-python\excel_input\MBV-AD-numbers.xlsx')
+
+print (df)
