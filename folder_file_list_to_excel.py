a9823e17bbff5054ebf3481113ef0d8e76fa5b86
Sziva Laszlo <Laszlo.Sziva@ansaldoenergia.com>
2020-09-14 08:44:24 +0200

    folder content to excel created and working

---
+++ folder_file_list_to_excel.py
@@ -0,0 +1,28 @@
+import pathlib
+from pathlib import Path
+import logging
+import os
+import xlwt
+from tempfile import TemporaryFile
+
+
+# find all files in all subfolders
+# folder = Path(r'\\AENXSTOR01.aen.ansaldo.it\share08\Auxiliary_Baden\AES\GT36 S5\Marghera spring support check\New_Drawings\MBV groups').glob('**/*')
+
+# find only pdfs in current folder - NO subfolders are searched through
+folder = Path(r'\\AENXSTOR01.aen.ansaldo.it\share08\Auxiliary_Baden\AES\GT36 S5\Marghera spring support check\New_Drawings\MBV groups').glob('*.pdf')
+
+listOfFiles = [x.name for x in folder if x.is_file()]
+
+# print (listOfFiles)
+
+book = xlwt.Workbook()
+sheet1 = book.add_sheet('sheet1')
+
+for i,e in enumerate(listOfFiles):
+    sheet1.write(i,1,e)
+
+name = "list_of_files_in_folder.xls"
+book.save(name)
+book.save(TemporaryFile())
+
--- pipe_support_dwg_numbers.py
+++ pipe_support_dwg_numbers.py
@@ -2,8 +2,36 @@ import pandas as pd
 import pathlib
 import logging
 
-print ("RAN")
-
 df = pd.read_excel(r'C:\Users\50000700\Python\Python_repos\multi-purpose-excel-wizardry-python\excel_input\MBV-AD-numbers.xlsx')
 
-print (df)
+# print (df)
+
+df_formatted = pd.DataFrame({'AD':['FIRST AD'],
+                   'KKS':['FIRST KKS']})
+
+# print (df_formatted)
+
+# Strip whitespace in all dataframe "cells"
+df = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+
+for (index, row_series) in df.iterrows():
+    if len(row_series) > 1:
+        current_AD = row_series["AD"]
+        #print (current_AD)
+        #row_series.drop(labels=["AD"])
+        for item in row_series.drop(labels=["AD"]):
+            #print (item)
+            df_current = pd.DataFrame({'AD':[current_AD],
+                   'KKS':[item]})
+            df_formatted = df_formatted.append(df_current)
+    # print (row_series[0])
+
+df_formatted = df_formatted.dropna()
+print (df_formatted)
+
+#writer = pd.ExcelWriter(r'C:\Users\50000700\Python\Python_repos\multi-purpose-excel-wizardry-python\excel_input\MBV-AD-numbers_formatted.xlsx')
+#df_formatted.to_excel(writer, sheet_name='Sheet1')
+
+df_formatted.to_excel(r'C:\Users\50000700\Python\Python_repos\multi-purpose-excel-wizardry-python\excel_input\MBV-AD-numbers_formatted.xlsx')
+
+#writer.save
